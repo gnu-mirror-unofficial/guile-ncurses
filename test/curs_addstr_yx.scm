16c1a29938deda0af67f6c60fef99bb9bfe6d886
Michael Gran <spk121@yahoo.com>
2016-05-21 15:54:48 -0700

    split curses_007_addstr.test into new style subtests
    * test/Makefile.am: add new tests
    * test/curses_007_addstr.test: removed
    * test/curses_addstr.scm: new test
    * test/curses_addstr_yx.scm: new test
    * test/curses_addstr_yxn.scm: new test

---
+++ test/curs_addstr_yx.scm
@@ -0,0 +1,31 @@
+;; Copyright 2009, 2010, 2016 Free Software Foundation, Inc.
+
+;; This file is part of Guile-Ncurses.
+
+;; Guile-Ncurses is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; Guile-Ncurses is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with Guile-Ncurses.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+(use-modules (test automake-test-lib)
+             (srfi srfi-1)
+             (ncurses curses))
+
+(automake-test
+ (let* ((mainwin (initscr)))
+   (addstr mainwin "hello" #:y 1 #:x 0)
+   (refresh mainwin)
+   (let ((w2 (instr mainwin #:y 1 #:x 0)))
+     (endwin)
+     (newline)
+     (format #t "instr: ~s~%" w2)
+     (string=? "hello" (string-take w2 (string-length "hello"))))))
