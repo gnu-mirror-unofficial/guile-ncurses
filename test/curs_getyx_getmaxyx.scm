faf928b93938d917d8d188cf5c015038b54ccf39
Michael Gran <spk121@yahoo.com>
2016-05-22 11:38:30 -0700

    split curses_017_getyx.test into new style subtests
    * test/Makefile.am (NARROWTESTS): modified
    * test/curses_017_getyx.test: removed
    * test/curs_getyx_getbegyx.scm: new test
    * test/curs_getyx_getmaxyx.scm: new test
    * test/curs_getyx_getparyx.scm: new test
    * test/curs_getyx_getyx.scm: new test

---
+++ test/curs_getyx_getmaxyx.scm
@@ -0,0 +1,41 @@
+;; Copyright 2009, 2010, 2016 Free Software Foundation, Inc.
+
+;; This file is part of Guile-Ncurses.
+
+;; Guile-Ncurses is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; Guile-Ncurses is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with Guile-Ncurses.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+(use-modules (test automake-test-lib)
+	     (srfi srfi-1)
+             (ncurses curses))
+
+(automake-test
+ (let* ((mainwin (initscr))
+	(win (newwin 5 6 7 8))
+	(win2 (subwin win 2 2 8 9)))
+   (clear mainwin)
+   (refresh mainwin)
+   (box win 0 0)
+   (box win2 0 0)
+   (touchwin win )
+   (refresh win2)
+   (refresh win)
+   (maybe-sleep 2)
+   (let ((myx (getmaxyx win)))
+     (endwin)
+     (newline)
+     (format #t "~s~%" win)
+     (format #t "~s~%" win2)
+     (format #t "~s~%" myx)
+     (list= eqv? '(5 6) myx))))
