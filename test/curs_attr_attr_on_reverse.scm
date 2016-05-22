1efe73711e313b5513d4702c973506c23a4d5c00
Michael Gran <spk121@yahoo.com>
2016-05-22 07:21:53 -0700

    Split curses_008_attr.test into new style subtests
    * test/Makefile.am (NARROWTESTS): updated
    * test/curses_008_attr.test: removed
    * test/curs_attr_attr_get.scm: new file
    * test/curs_attr_attr_off_underline.scm: new file
    * test/curs_attr_attr_on_blink.scm: new file
    * test/curs_attr_attr_on_bold.scm: new file
    * test/curs_attr_attr_on_dim.scm: new file
    * test/curs_attr_attr_on_invis.scm: new file
    * test/curs_attr_attr_on_protect.scm: new file
    * test/curs_attr_attr_on_reverse.scm: new file
    * test/curs_attr_attr_on_standout.scm: new file
    * test/curs_attr_attr_on_underline.scm: new file
    * test/curs_attr_attr_set_normal.scm: new file
    * test/curs_attr_attr_set.scm: new file
    * test/curs_attr_chgat.scm: new file
    * test/curs_attr_chgat_yx.scm: new file
    * test/curs_attr_color_set.scm: new file
    * test/curs_attr_standend.scm: new file
    * test/curs_attr_standout.scm: new file

---
+++ test/curs_attr_attr_on_reverse.scm
@@ -0,0 +1,37 @@
+;; Copyright 2009, 2010, 2011, 2016 Free Software Foundation, Inc.
+
+;; This file is part of Guile-Ncurses.
+
+;; Guile-Ncurses is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; Guile-Ncurses is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with Guile-Ncurses.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+(use-modules (test automake-test-lib)
+             (srfi srfi-1)
+             (ice-9 format)
+             (ncurses curses))
+
+(automake-test
+ (let ((win (initscr)))
+   (start-color!)
+   (clear win)
+   (attr-on! win A_REVERSE)
+   (addstr win "x" #:y 0 #:x 0)
+   (refresh win)
+   (let ((x2 (inch win #:y 0 #:x 0)))
+     (endwin)
+     (newline)
+     (format #t "inch: ~s~%" x2)
+     (format #t "A_REVERSE: ~s~%" A_REVERSE)
+     (format #t "xchar-attr: ~s~%" (xchar-attr x2))
+     (equal? A_REVERSE (xchar-attr x2)))))
