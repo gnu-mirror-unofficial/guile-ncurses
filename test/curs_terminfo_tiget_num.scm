d76aa80a6ead561ef7504694c48ae52cf19f99d2
Mike Gran <spk121@yahoo.com>
2016-05-25 18:26:03 -0400

    Add tests for terminfo functions
    * test/Makefile.am (NARROWTESTS): update
    * test/curs_terminfo_setupterm.scm: new test
    * test/curs_terminfo_setupterm_bad: new test
    * test/curs_terminfo_tiget_bool.scm: new test
    * test/curs_terminfo_tiget_num.scm: new test
    * test/curs_terminfo_tiget_string.scm: new test
    * test/curs_terminfo_tiget_unknown: new test

---
+++ test/curs_terminfo_tiget_num.scm
@@ -0,0 +1,28 @@
+;; Copyright 2016 Free Software Foundation, Inc.
+
+;; This file is part of Guile-Ncurses.
+
+;; Guile-Ncurses is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; Guile-Ncurses is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with Guile-Ncurses.  If not, see
+;; <http://www.gnu.org/licenses/>.
+(use-modules (test automake-test-lib)
+	     (ncurses curses))
+
+(automake-test
+ (let* ((t (setupterm "xterm"))
+	(ret (tiget "colors")))
+   (format #t "setupterm xterm: ~s" t)
+   (newline)
+   (format #t "tiget colors: ~S" ret)
+   (newline)
+   (eqv? ret 8)))
